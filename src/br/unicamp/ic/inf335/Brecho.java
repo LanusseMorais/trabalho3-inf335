42bf66db242551c26bfb868d949d5a213e3f329f
thalita <thalita@softnex.com.br>
2023-04-28 20:01:12 -0300

    fix: Correção para adicionar todos os produtos na lista

--- src/br/unicamp/ic/inf335/Brecho.java
+++ src/br/unicamp/ic/inf335/Brecho.java
@@ -10,14 +10,19 @@ public class Brecho {
 	private static ArrayList<ProdutoBean> produtos = new ArrayList<ProdutoBean>();
 
 	public static void main(String[] args) {
-		ProdutoBean nProg = new ProdutoBean("CD00001", "Celular Galaxy S10", "128 Gb, Preto, com Carregador", 1250.0,
+		ProdutoBean nProg1 = new ProdutoBean("CD00001", "Celular Galaxy S10", "128 Gb, Preto, com Carregador", 1250.0,
 				"Poucos riscos, estado de novo.");
-		nProg = new ProdutoBean("CD00002", "Prod 2 ...", "Bla Bla Bla", 1100.0, "Bla Bla Bla");
-		nProg = new ProdutoBean("CD00003", "Prod 3 ...", "Bla Bla Bla", 120.0, "Bla Bla Bla");
-		nProg = new ProdutoBean("CD00004", "Prod 4 ...", "Bla Bla Bla", 1300.0, "Bla Bla Bla");
-		nProg = new ProdutoBean("CD00005", "Prod 5 ...", "Bla Bla Bla", 9400.0, "Bla Bla Bla");
-		nProg = new ProdutoBean("CD00006", "Prod 6 ...", "Bla Bla Bla", 1500.0, "Bla Bla Bla");
-		produtos.add(nProg);
+		ProdutoBean nProg2 = new ProdutoBean("CD00002", "Prod 2 ...", "Bla Bla Bla", 1100.0, "Bla Bla Bla");
+		ProdutoBean nProg3 = new ProdutoBean("CD00003", "Prod 3 ...", "Bla Bla Bla", 120.0, "Bla Bla Bla");
+		ProdutoBean nProg4 = new ProdutoBean("CD00004", "Prod 4 ...", "Bla Bla Bla", 1300.0, "Bla Bla Bla");
+		ProdutoBean nProg5 = new ProdutoBean("CD00005", "Prod 5 ...", "Bla Bla Bla", 9400.0, "Bla Bla Bla");
+		ProdutoBean nProg6 = new ProdutoBean("CD00006", "Prod 6 ...", "Bla Bla Bla", 1500.0, "Bla Bla Bla");
+		produtos.add(nProg1);
+		produtos.add(nProg2);
+		produtos.add(nProg3);
+		produtos.add(nProg4);
+		produtos.add(nProg5);
+		produtos.add(nProg6);
 
 		// Imprime produtos
 		for (int i = 0; i < produtos.size(); i++) {
